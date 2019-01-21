3af953c067e0eefbe0c0e6dd6119bb8458127899
Timothy Harley <timothy.d.harley@gmail.com>
2019-01-21 15:22:27 -0600

    added authentication button and apiKeys

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MyNavbar from '../components/myNavbar/myNavbar';
+import connection from '../helpers/data/connection';
+import Auth from '../components/auth/auth';
 import './App.scss';
 
 class App extends Component {
+  componentDidMount() {
+    connection();
+  }
+
   render() {
     return (
       <div className="App">
         <MyNavbar />
+        <Auth />
       </div>
     );
   }
