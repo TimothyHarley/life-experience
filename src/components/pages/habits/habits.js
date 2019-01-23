13904f22043d531c5c538fcfe77c23978b07ebd4
Timothy Harley <timothy.d.harley@gmail.com>
2019-01-22 21:06:38 -0600

    console logged habitData

--- src/components/pages/habits/habits.js
+++ src/components/pages/habits/habits.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import './habits.scss';
+import habitRequests from '../../../helpers/data/habitRequests';
 
 class Habits extends React.Component {
+  state = {
+    habits: [],
+  }
+
+  componentDidMount() {
+    habitRequests.getHabits()
+      .then((habits) => {
+        this.setState({ habits });
+      });
+  }
+
   render() {
+    const { habits } = this.state;
+    console.log(habits);
+
     return (
       <div className="habits">
         <h2>Habits</h2>
