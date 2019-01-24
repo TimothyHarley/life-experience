ade2b9598d9c7f0a0894b8288b42bddd482a4ab5
Timothy Harley <timothy.d.harley@gmail.com>
2019-01-24 15:13:05 -0600

    categories load, but in the wrong place. . .

--- src/components/pages/habits/habits.js
+++ src/components/pages/habits/habits.js
@@ -1,19 +1,45 @@
 import React from 'react';
+import {
+  TabContent,
+  TabPane,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
+import classnames from 'classnames';
 import './habits.scss';
 import SingleHabit from './singleHabit';
 import habitRequests from '../../../helpers/data/habitRequests';
 
 class Habits extends React.Component {
-  state = {
-    habits: [],
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      activeTab: 'fitness',
+      habits: [],
+    };
   }
 
-  componentDidMount() {
-    habitRequests.getHabits('fitness')
-      .then((habits) => {
-        this.setState({ habits });
+  toggle(tab) {
+    if (this.state.activeTab !== tab) {
+      this.setState({
+        activeTab: tab,
       });
+      habitRequests.getHabits(this.state.activeTab)
+        .then((habits) => {
+          this.setState({ habits });
+        });
+    }
   }
+
+  // componentDidMount() {
+  //   habitRequests.getHabits(this.state.activeTab)
+  //     .then((habits) => {
+  //       this.setState({ habits });
+  //     });
+  // }
 
   render() {
     const { habits } = this.state;
@@ -26,9 +52,37 @@ class Habits extends React.Component {
 
     return (
       <div className='habits'>
-        <h2>Habits</h2>
-        <div className='habitCardContainer d-flex flex-wrap justify-content-center'>
-          {singleHabitCards}
+        <div>
+          <Nav tabs>
+            <NavItem>
+              <NavLink
+                className={classnames({ active: this.state.activeTab === 'fitness' })}
+                onClick={() => { this.toggle('fitness'); }}
+              >
+                Fitness
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                className={classnames({ active: this.state.activeTab === 'academic' })}
+                onClick={() => { this.toggle('academic'); }}
+              >
+                Academic
+              </NavLink>
+            </NavItem>
+          </Nav>
+          <TabContent activeTab={this.state.activeTab}>
+            <TabPane tabId="fitness">
+              <div className='habitCardContainer d-flex flex-wrap justify-content-center'>
+                {singleHabitCards}
+              </div>
+            </TabPane>
+            <TabPane tabId="academic">
+              <div className='habitCardContainer d-flex flex-wrap justify-content-center'>
+                {singleHabitCards}
+              </div>
+            </TabPane>
+          </TabContent>
         </div>
       </div>
     );
