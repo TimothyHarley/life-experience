351a70e8144a2bfbcc5c457c03a556564217d6b5
Timothy Harley <timothy.d.harley@gmail.com>
2019-02-02 14:02:00 -0600

    added seed data and profile content

--- src/components/pages/profile/profile.js
+++ src/components/pages/profile/profile.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './profile.scss';
 
+import authRequests from '../../../helpers/data/authRequests';
+
 class Profile extends React.Component {
+
   render() {
     return (
       <div className="profile">
-        <h2>Profile Page</h2>
+        <h2>{authRequests.userProfile().displayName}</h2>
+        <img src={authRequests.userProfile().photoURL} alt="profile"/>
       </div>
     );
   }
